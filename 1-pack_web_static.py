325d0179ea866eb1357b437f61b9be460f18bcf7
jeniffervp <737@holbertonschool.com>
2019-08-20 04:21:16 +0000

    [FEature]make a tgz file using fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" create a tgz file """
+from datetime import datetime
+from fabric.api import local
+
+def do_pack():
+    """ Function to make a pack of a directory on local"""
+
+    local('mkdir -p versions')
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    final = 'versions/web_static_{}.tgz'.format(date)
+
+    try:
+        local('tar -czvf {} web_static'.format(final))
+        return final
+    except:
+        return None
