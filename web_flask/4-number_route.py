5812d05e7ca6004a3e91a7712cc92a14de9a1f21
jeniffervp <737@holbertonschool.com>
2019-09-03 23:03:16 +0000

    [Feature]validate if a int is given

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask
+
+
+hbnb = Flask(__name__)
+hbnb.url_map.strict_slashes = False
+
+
+@hbnb.route('/')
+def hi_hbnb():
+    return "Hello HBNB!"
+
+
+@hbnb.route('/hbnb')
+def only_hbnb():
+    return "HBNB"
+
+
+@hbnb.route('/c/<text>')
+def some_text(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@hbnb.route('/python')
+@hbnb.route('/python/<text>')
+def py_text(text="is_cool"):
+        return "Python {}".format(text.replace("_", " "))
+
+
+@hbnb.route('/number/<int:n>')
+def some_number(n):
+    return "{} is a number".format(n)
+
+if __name__ == "__main__":
+    hbnb.run(host="0.0.0.0", port="5000")
