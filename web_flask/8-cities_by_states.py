814e31f882fd4e75c46b1d2e876d42b1eeceae7b
jeniffervp <737@holbertonschool.com>
2019-09-05 00:05:49 +0000

    [Feature]display the cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def states():
+    var = list(storage.all(State).values())
+    return render_template("8-cities_by_states.html", var=var)
+
+
+@app.teardown_appcontext
+def closer(err):
+    """ close app context """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
